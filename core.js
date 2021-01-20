abc51b632b35b6510785cc59c85e9640af27914c
EricRootLee <erickiokom@gmail.com>
2021-01-21 00:28:05 +0300

    SMall modification

--- core.js
+++ core.js
@@ -23,14 +23,12 @@ class Block {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
         const hash = Block.hash(timestamp, lastHash, data);
-
         return new this(timestamp, lastHash, hash, data);
     }
     //hash
     static hash(timestamp, lastHash, data) {
         return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
-
     //blockhash
 
     static blockHash(block) {
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //block import
 const Block = require('./core');
-const Blc = require('./blockchain')
 //const block = new Block('foo', 'bar', 'zoo', 'bar');
 //console.log(block.toString());
 //console.log(Block.genesis().toString());
+let bc = [Block.genesis()]
 for(i = 0 ; i < 10 ; i ++){
-   // const min1 = Block.mineBlock(Block.genesis(),'foo');
-    const blc = Blc.addBlock('muli')
-    console.log(min1.toString());
+    const min1 = Block.mineBlock(bc[bc.length-1],`foo ${i}`);
+    bc.push(min1)
+    console.log(bc[i].toString());
 }
