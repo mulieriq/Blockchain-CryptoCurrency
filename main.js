2e82cffaaf5aa6998b16b1055507afe849c6e501
EricRootLee <erickiokom@gmail.com>
2021-01-21 00:02:14 +0300

    Added blc

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //block import
 const Block = require('./core');
+const Blc = require('./blockchain')
 //const block = new Block('foo', 'bar', 'zoo', 'bar');
 //console.log(block.toString());
 //console.log(Block.genesis().toString());
 for(i = 0 ; i < 10 ; i ++){
-    const min1 = Block.mineBlock(Block.genesis(),'foo');
+   // const min1 = Block.mineBlock(Block.genesis(),'foo');
+    const blc = Blc.addBlock('muli')
     console.log(min1.toString());
 }
